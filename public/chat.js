c56431d1649a9f90a75bba12e5c6128d66c0ddc8
John Crooks <johnmcrooks@gmail.com>
2016-06-15 11:57:51 -0400

    Week 5 Day 3 - June 15: Displaying posted messages, semi-functional

--- public/chat.js
+++ public/chat.js
@@ -8,8 +8,30 @@ function addMessage(){
         "/add-message",
         JSON.stringify(message),    /* explicitly convert the object to JSON */
         function(data) {            /* Call back function required for a post route in J.S.   */
-            alert("success");
+            $("#text").val("");
+            $("#author").val("");
         }
 
     );
 }
+
+function getMessages(){
+    $.get(
+        "/get-messages",
+        function(data){         /* Retrieves json information from the server */
+            $("#messages").empty();   /* clears out the messages div so that the information doesn't multiply on every request */
+            var messages = JSON.parse(data);        /* parse the info into a variable */
+            for(var i in messages){             /* create a loop to grab author and text out */
+                var author = messages[i].author;
+                var text = messages[i].text;
+
+                var elem = $("<div>")           /* create a div dynamically for setting test and displaying shit */
+                elem.text(author + ": " + text) /* within the div tag we are adding the information grabbed above */
+                $("#messages").append(elem);    /* appending the div to Messages which we created in the HTML file */
+            }
+        }
+    )
+}
+setInterval(getMessages, 100);
+
+getMessages();
